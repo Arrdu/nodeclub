145f8de64c1091269f588ee982a7744acec60f82
derekhu <251877979@qq.com>
2015-12-06 16:18:19 +0800

    c

--- config.js
+++ config.js
@@ -118,9 +118,9 @@ var config = {
     ['u3d', 'VR应用设计'],
     ['opt', 'VR应用优化'],
     ['videomake', 'VR视频制作'],
-    ['cardboardsdk', 'CardboardSDK问答'],
     ['dev', 'VR外围设备'],
     ['publish', 'VR作品交流'],
+    ['cardboardsdk', 'CardboardSDK问答'],
   ],
 
   // 极光推送
